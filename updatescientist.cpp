8ecf8cb11ec57540eea0f6f13c49608891be2e2e
Arnar Ársælsson <arnarfreyr@Arnars-MacBook-Pro-2.local>
2016-12-14 12:00:40 +0000

    update scientist

--- updatescientist.cpp
+++ updatescientist.cpp
@@ -12,3 +12,33 @@ updatescientist::~updatescientist()
 {
     delete ui;
 }
+
+void updatescientist::setName(const std::string s)
+{
+    ui->updateNameInput->setText(QString::fromStdString(s));
+}
+
+void updatescientist::setGender(const std::string g)
+{
+    if(g == "F")
+    {
+        ui->updateGenderBox->setCurrentText(QString::fromStdString("Female"));
+
+    }
+    else if(g == "M")
+    {
+        ui->updateGenderBox->setCurrentText(QString::fromStdString("Male"));
+    }
+}
+
+void updatescientist::setBirthYear(const int bY)
+{
+    QDate b(bY, 1, 1);
+    ui->updateBirthYear->setDate(b);
+}
+
+void updatescientist::setDeathYear(const int dY)
+{
+    QDate d(dY, 1, 1);
+    ui->updateDeathYear->setDate(d);
+}
--- updatescientist.h
+++ updatescientist.h
@@ -15,6 +15,11 @@ public:
     explicit updatescientist(QWidget *parent = 0);
     ~updatescientist();
 
+    void setName(const std::string s);
+    void setGender(const std::string g);
+    void setBirthYear(const int bY);
+    void setDeathYear(const int dY);
+
 private:
     Ui::updatescientist *ui;
 };
